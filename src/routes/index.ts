0f1604f8c0bea341ad7a8e95f8dfc4617d826cbc
Leonardo Silva <f.leobrito@gmail.com>
2021-02-12 09:39:30 -0300

    :construction: Work in progress

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,12 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
 
 routes.get('/', (request, response) =>
   response.json({ message: "It's work!" }),
 );
-
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json({ user });
-});
 
 export default routes;
